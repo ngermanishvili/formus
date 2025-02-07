2575159f1bd32594de527f099d389b943cf08b21
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2025-02-07 16:40:48 +0400

    upd

--- components/homes/home-5/Hero.jsx
+++ components/homes/home-5/Hero.jsx
@@ -194,55 +194,7 @@ export default function Hero() {
           </div>
         </div>
         <div className="box-services-banner">
-          <div className="container-sub px-4 md:px-8">
-            <div className="box-swiper">
-              {data.projects.length > 0 && (
-                <Swiper
-                  {...baseSettings}
-                  breakpoints={breakpoints}
-                  loop={data.projects.length > 4}
-                  className="swiper-container swiper-group-4 pb-0"
-                >
-                  {data.projects.map((project) => (
-                    <SwiperSlide key={project.id} className="swiper-slide">
-                      <div className="cardService cardServiceStyle3">
-                        <Link
-                          href={`/projects/${project.id}/${getProjectSlug(
-                            project
-                          )}`}
-                        >
-                          <div className="cardImage">
-                            <Image
-                              width={370}
-                              height={400}
-                              src={project.main_image_url}
-                              alt={
-                                currentLang === "ge"
-                                  ? project.title_ge
-                                  : project.title_en
-                              }
-                              priority
-                              className="w-full h-full object-cover"
-                            />
-                          </div>
-                          <div className="cardInfo">
-                            <h3
-                              suppressHydrationWarning
-                              className="cardTitle text-base md:text-lg lg:text-xl color-white mb-2 md:mb-3"
-                            >
-                              {currentLang === "ge"
-                                ? project.title_ge
-                                : project.title_en}
-                            </h3>
-                          </div>
-                        </Link>
-                      </div>
-                    </SwiperSlide>
-                  ))}
-                </Swiper>
-              )}
-            </div>
-          </div>
+          <div className="container-sub px-4 md:px-8"></div>
         </div>
       </div>
     </section>

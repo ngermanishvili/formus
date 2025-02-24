905fd40757a48c7f064ecf141f3169a9e519ebc3
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2025-02-24 11:34:44 +0400

    upd

--- app/[locale]/(projects)/projects/(components)/hero-content.jsx
+++ app/[locale]/(projects)/projects/(components)/hero-content.jsx
@@ -136,7 +136,7 @@ const ProjectContent = ({ id }) => {
           </div>
           <div className="absolute inset-0 bg-black/50" />
           <div className="absolute inset-0 flex items-center">
-            <div className="container mx-auto px-4">
+            <div className="w-full mx-auto  max-w-[1000px]">
               <p className="font-firago text-sm text-white">
                 {projectData.location}
               </p>
